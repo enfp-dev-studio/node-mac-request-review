07d3b6f06a51ba2932519d25c4db057e8f69b96c
Jake Roh <enfpdev@gmail.com>
2022-12-29 18:24:38 +0900

    bug fix

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-// const native = require("bindings")("request_review.node");
-
-// module.exports = {
-//   requestReview: native.requestReview,
-// };
-
 var binding = require("node-gyp-build")(__dirname);
 
-module.exports = binding;
+module.exports = {
+  requestReview: binding.requestReview,
+};
